248727a9ff6baa831354f6af10103ac8c1516cfa
Nikolay Dubatov <nikolay.dubatov@yandex.ru>
2026-05-08 17:40:18 +0700

    Add home page

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
-
+@app.route('/')
+@app.route('/home')
+def home() -> str:
+    return render_template('home.html', title="Home")
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', 5000, debug=False)
