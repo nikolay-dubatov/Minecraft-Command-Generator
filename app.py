5285911f427973f8574361f559f74897bd81b9f9
Nikolay Dubatov <nikolay.dubatov@yandex.ru>
2026-05-27 15:16:48 +0700

    Getting full list of items

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5000, debug=True)
+    app.run('0.0.0.0', 5000, debug=False)
